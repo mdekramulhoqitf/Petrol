366a0c5804e3efbbdbb8cf469c8efc0175214ec5
MD Ekramul Hoq <103028411+mdekramulhoqitf@users.noreply.github.com>
2022-09-12 00:02:19 +0200

    Code-plm

---
+++ js/teilwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        red: "#F40404",
+        overlay: "rgba(0,0,0,0.6)",
+      },
+      maxWidth: {
+        container: "1140px",
+      },
+      fontFamilly: {
+        poppins: "Poppins",
+      },
+      backgroundImage: {
+        banner: "url('images/banner.png')",
+        serviceimages: "url('images/serviceimages.png')",
+      },
+    },
+  },
+};
